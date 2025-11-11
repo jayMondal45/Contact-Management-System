b4cbf884c853301521673d077f956b6ac0c13e75
Joy Mondal <jaymondals953@gmail.com>
2025-11-11 21:26:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -63,4 +63,5 @@ def all_clients():
     return render_template("clients.html", clients=all_clients)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
